908c2ebb397bc64d297904448b6a350d6bef4b03
Krish-Dev <krishtiwari726@gmail.com>
2026-01-18 17:53:55 +0530

    Add a simple Java program for user input

---
+++ Variable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Day2_Variables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
